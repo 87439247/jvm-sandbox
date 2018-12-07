4ed3598d30bab376a82ec38f092762309fec98a7
yuanyue <yuanyue@staff.hexun.com>
2018-12-07 11:42:56 +0800

    手工埋点

---
+++ sandbox-debug-module/src/main/java/com/alibaba/jvm/sandbox/module/debug/CatManualEmbeddingModule.java
@@ -0,0 +1,96 @@
+package com.alibaba.jvm.sandbox.module.debug;
+
+import com.alibaba.jvm.sandbox.api.Information;
+import com.alibaba.jvm.sandbox.api.Module;
+import com.alibaba.jvm.sandbox.api.listener.ext.Advice;
+import com.alibaba.jvm.sandbox.api.listener.ext.AdviceListener;
+import com.alibaba.jvm.sandbox.api.listener.ext.EventWatchBuilder;
+import com.alibaba.jvm.sandbox.api.resource.ModuleEventWatcher;
+import com.dianping.cat.Cat;
+import com.dianping.cat.message.Transaction;
+import org.kohsuke.MetaInfServices;
+
+import javax.annotation.Resource;
+
+import static com.alibaba.jvm.sandbox.module.debug.util.CatFinishUtil.finish;
+import static com.alibaba.jvm.sandbox.module.debug.util.MethodUtils.invokeMethod;
+import static com.alibaba.jvm.sandbox.module.debug.util.UrlUtils.rebuildPath;
+
+@MetaInfServices(Module.class)
+@Information(id = "cat-manual-embedding", version = "0.0.1", author = "yuanyue@staff.hexun.com")
+public class CatManualEmbeddingModule extends CatModule {
+
+    @Resource
+    private ModuleEventWatcher moduleEventWatcher;
+
+    @Override
+    public void loadCompleted() {
+        monitorManualEmbedding();
+    }
+
+
+    /**
+     * public static void logMetricForCount(String name)
+     * <p>
+     * public static void logMetricForCount(String name, int quantity)
+     */
+    private void monitorManualEmbedding() {
+
+        new EventWatchBuilder(moduleEventWatcher)
+                .onClass("com.hexun.**.Cat")
+                .includeBootstrap()
+                .onBehavior("logMetricForCount")
+                .withParameterTypes("java.lang.String")
+
+                .onWatch(new AdviceListener() {
+
+                    @Override
+                    public void before(Advice advice) throws Throwable {
+                        // nothing
+                    }
+
+                    @Override
+                    public void afterReturning(Advice advice) {
+                        int count = 1;
+                        if (advice.getParameterArray().length >= 2) {
+                            count = (int) advice.getParameterArray()[1];
+                        }
+                        Cat.logMetricForCount((String) advice.getParameterArray()[0], count);
+                    }
+
+                    @Override
+                    public void afterThrowing(Advice advice) {
+                        // nothing
+                    }
+                });
+
+        new EventWatchBuilder(moduleEventWatcher)
+                .onClass("com.hexun.**.Cat")
+                .includeBootstrap()
+                .onBehavior("logMetricForDuration")
+                .withParameterTypes("java.lang.String", long.class.getName())
+
+                .onWatch(new AdviceListener() {
+
+                    @Override
+                    public void before(Advice advice) throws Throwable {
+                        // nothing
+                    }
+
+                    @Override
+                    public void afterReturning(Advice advice) {
+                        Cat.logMetricForDuration((String) advice.getParameterArray()[0], (int) advice.getParameterArray()[1]);
+                    }
+
+                    @Override
+                    public void afterThrowing(Advice advice) {
+                        // nothing
+                    }
+                });
+    }
+
+    @Override
+    String getCatType() {
+        return "HttpClient";
+    }
+}
