1943ed06958e005f3329863a4da08c2a41b80d35
yuanyue <yuanyue@staff.hexun.com>
2018-12-10 10:09:39 +0800

    domain 从环境变量中取

--- sandbox-debug-module/src/main/java/com/alibaba/jvm/sandbox/module/debug/CatModule.java
+++ sandbox-debug-module/src/main/java/com/alibaba/jvm/sandbox/module/debug/CatModule.java
@@ -3,12 +3,18 @@ package com.alibaba.jvm.sandbox.module.debug;
 import com.alibaba.jvm.sandbox.api.LoadCompleted;
 import com.alibaba.jvm.sandbox.api.Module;
 import com.dianping.cat.Cat;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public abstract class CatModule implements Module, LoadCompleted {
     static {
-        Cat.initializeByDomainForce("cat111");
+        final String domainKey = "catdomain";
+        String catDomain = System.getenv(domainKey);
+        if (StringUtils.isBlank(catDomain)) {
+            catDomain = System.getProperty(domainKey, "cat111");
+        }
+        Cat.initializeByDomainForce(catDomain);
     }
 
     protected Logger stLogger = LoggerFactory.getLogger(this.getClass());
