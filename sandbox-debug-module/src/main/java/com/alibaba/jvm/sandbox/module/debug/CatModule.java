a6e2402af3a695275f71abdadc0386eb1bd353b1
yuanyue <yuanyue@staff.hexun.com>
2018-12-12 22:52:01 +0800

    change to stringbuilder

--- sandbox-debug-module/src/main/java/com/alibaba/jvm/sandbox/module/debug/CatModule.java
+++ sandbox-debug-module/src/main/java/com/alibaba/jvm/sandbox/module/debug/CatModule.java
@@ -2,6 +2,7 @@ package com.alibaba.jvm.sandbox.module.debug;
 
 import com.alibaba.jvm.sandbox.api.LoadCompleted;
 import com.alibaba.jvm.sandbox.api.Module;
+import com.alibaba.jvm.sandbox.module.debug.util.IpUtils;
 import com.dianping.cat.Cat;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -14,14 +15,18 @@ public abstract class CatModule implements Module, LoadCompleted {
     static Logger internalLogger = LoggerFactory.getLogger(CatModule.class);
 
     static {
-        internalLogger.error("env size=" + System.getenv().size());
+        StringBuilder envBuider = new StringBuilder();
+        envBuider.append("\nenv size=").append(System.getenv().size());
         for (String envKey : System.getenv().keySet()) {
-            internalLogger.error("env key=" + envKey + ";value=" + System.getenv(envKey));
+            envBuider.append("\nenv key=").append(envKey).append(";value=").append(System.getenv(envKey));
         }
-        internalLogger.error("prop size=" + System.getProperties().size());
+        envBuider.append("\nprop size=").append(System.getProperties().size());
         for (Object propertyKey : System.getProperties().keySet()) {
-            internalLogger.error("prop key=" + propertyKey + ";value=" + System.getProperty((String) propertyKey));
+            envBuider.append("\nprop key=").append(propertyKey).append(";value=").append(System.getProperty((String) propertyKey));
         }
+        envBuider.append("\nIP=").append(IpUtils.getIp());
+        envBuider.append("\nHOSTNAME=").append(IpUtils.getHostName());
+        internalLogger.error(envBuider.toString());
         final String domainKey = "catdomain";
         CAT_DOMAIN = getConfigFromEnv(domainKey, "catdemo");
         Cat.initializeByDomainForce(CAT_DOMAIN);
